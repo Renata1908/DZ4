27897e62b185f9d8299b49ab502ba1557283a1a5
Renata19 <renatalsa@mail.ru>
2022-09-30 15:51:09 +0700

    29

---
+++ task29/Program.cs
@@ -0,0 +1,20 @@
+﻿int ReadNumber(string message)
+{
+Console.Write(message);
+return Convert.ToInt32(Console.ReadLine());
+}
+
+int SumDigits(int number)
+{
+int sum = 0;
+while (number != 0)
+{
+sum = sum + number % 10;
+number = number/10;
+}
+return sum;
+}
+
+int number = ReadNumber("Введите число: ");
+int result = SumDigits(number);
+Console.WriteLine($"Сумма цифр в числе {number}: {Math.Abs(result)}");
